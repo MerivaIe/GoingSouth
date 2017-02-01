43613357fb9efbaf1810358b79e6f6823ae4673c
Merivale <bm9269@my.bristol.ac.uk>
2017-02-01 21:21:22 +0000

    End of day 1: Made crude trains and platforms and baked a navmesh based on the platforms. Also crude navmeshagent and target. Couldn't get jankphysics to work, want to have agent fly off when hit by train. Do tomorrow.

---
+++ Assets/People/Person.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Person : MonoBehaviour {
+
+	private NavMeshAgent nmAgent;
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent <Rigidbody> ();
+		nmAgent = GetComponent <NavMeshAgent>();
+		nmAgent.SetDestination (GameObject.Find("Target").transform.position);
+	}
+	
+	void Update () {
+		//nmAgent.SetDestination (GameObject.Find("Target").transform.position);
+
+		RaycastHit hit;
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit))
+				nmAgent.SetDestination(hit.point);
+
+		}
+
+	}
+
+	void OnCollisionEnter(Collision coll) {
+		Debug.Log ("Colliding");
+		if (coll.gameObject.GetComponent <Train> ()) {
+			rb.isKinematic = false;
+			nmAgent.enabled = false;
+			Debug.Log ("Train script found in object");
+
+		} else if (coll.gameObject.GetComponentInParent <Train> ()) {
+
+			rb.isKinematic = false;
+			nmAgent.enabled = false;
+			Debug.Log ("Train script found in parent object");
+		}
+	}
+}
---
+++ Assets/Trains/Train.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour {
+
+	public float speed = 10f;
+	private Rigidbody rb;
+	private bool travelRight = true;
+	private Vector3 newPos;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponentInChildren <Rigidbody> ();
+		newPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < -50f) {
+			travelRight = true;
+		} else if (transform.position.x > 50f) {
+			travelRight = false;
+		}
+
+		if (travelRight) {
+			print ("Setting speed right" + Vector3.right * speed);
+			newPos.x = transform.position.x + speed * Time.deltaTime;
+			transform.position = newPos;
+			//rb.velocity = Vector3.right * speed;
+		} else {
+			print ("Setting speed left" + Vector3.left * speed);
+			newPos.x = transform.position.x - speed * Time.deltaTime;
+			transform.position = newPos;
+			//rb.velocity = Vector3.left * speed;
+		}
+	}
+}
