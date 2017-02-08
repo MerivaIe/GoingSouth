11331951c13137f7675e9d5ff7f84b7554e58cd9
Merivale <bm9269@my.bristol.ac.uk>
2017-02-08 17:23:59 +0000

    Got a fairly nice crushing system going for boarding. Now testing different funneling collider and crushing collider shapes.

---
+++ Assets/Crusher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crusher : MonoBehaviour {
+
+	public Vector3 crushingForce;
+
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent <Rigidbody> ();
+	}
+	
+	void FixedUpdate () {
+		rb.AddForce (crushingForce);
+	}
+}
