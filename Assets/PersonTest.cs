e6c60d07f36490a9ad0a3119d2cbb8e3494bb61c
Merivale <bm9269@my.bristol.ac.uk>
2017-02-14 10:57:04 +0000

    In BoardingTest scene we have a situation that should be used in Menu scene maybe. Levitating crowd of people (in gravity) swarming around each other. Half/half split aiming for two targets

---
+++ Assets/PersonTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonTest : MonoBehaviour {
+
+	private static GameObject target1, target2, myTarget;
+	private Rigidbody rb;
+	private float myForce = 2f;
+	private bool pushPlease = true;
+
+	void Start () {
+		rb = GetComponent <Rigidbody> ();
+		if (!target1) {
+			target1 = GameObject.Find ("Target (1)");
+		}
+		if (!target2) {
+			target2 = GameObject.Find ("Target (2)");
+		}
+		myTarget = (Random.value > 0.5f) ? target1 : target2;
+	}
+	
+	void FixedUpdate () {
+		if (pushPlease) {
+			rb.AddForce (myForce * (myTarget.transform.position - transform.position));
+		}
+	}
+
+	void OnTriggerEnter(Collider coll) {
+		if (coll.gameObject.GetComponent <Train> ()) {
+			pushPlease = false;
+		}
+	}
+}
