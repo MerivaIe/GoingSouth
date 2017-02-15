7df6a3740cc66db64ee3bcd4e3e2319815d510e6
Merivale <bm9269@my.bristol.ac.uk>
2017-02-15 12:50:08 +0000

    People at platform distribute according to Poisson disc. Got people boarding using force alone. Now people select target location closest to them.

--- Assets/People/Person.cs
+++ Assets/People/Person.cs
@@ -8,106 +8,107 @@ using UnityEngine.AI;
 public class Person : MonoBehaviour {
 
 	/// <summary>
-	/// MovingToPlatform: nmAgent control to centre of platform | WrongPlatform: nmAgent control to waiting area | MovingToDoor: nmAgent control to one door location | Boarding: physics control avoidance of others
+	/// PersonStatus: MovingToPlatform=nmAgent control to centre of platform | WrongPlatform=nmAgent control to waiting area | MovingToDoor=nmAgent control to one door location | Boarding=physics control avoidance of others
 	/// </summary>
-	public enum PersonStatus{MovingToPlatform,WrongPlatform,MovingToDoor,ReadyToBoard,Boarding,Alighted,Compromised}
+	public enum PersonStatus{MovingToPlatform,WrongPlatform,ReadyToBoard,Boarding,Boarded,Alighted,Compromised}
 	public PersonStatus status; 
 	public string destination{ get; private set; }
 	public Platform currentPlatform;
-	public float jostlingStrength = 0.25f, centreOfMassYOffset = -0.5f, checkProximityEvery = 1f, sqrMagDeathVelocity = 1f;
+	public float  centreOfMassYOffset = -0.5f, sqrMagDeathVelocity = 1f, boardingForce = 2f;
 
 	private NavMeshAgent nmAgent;
 	private Rigidbody rb;
-	private Vector3[] proximityDirections = new Vector3[8];
-	private Vector3 platformTarget;
-	private float nextProximityCheck, proximityDistance;
+	private Vector3 platformTarget, trainTarget;
 
 	void Start () {
 		rb = GetComponent <Rigidbody> ();
 		nmAgent = GetComponent <NavMeshAgent>();
 		destination = "Bristol";
 		rb.centerOfMass = new Vector3(0f,centreOfMassYOffset,0f);
-		for (int i=0;i<proximityDirections.Length;i++) {
-			proximityDirections [i] = new Vector3 (Mathf.Cos (i*Mathf.PI/4f),0f,Mathf.Sin (i*Mathf.PI/4f));		//using square wave function to calculate 8 directions at compass points
-		}
 	}
 
 	void FixedUpdate() {
-		//if (currentPlatform && currentPlatform.incomingTrain.status == Train.TrainStatus.BoardingTime && status == PersonStatus.MovingToDoor) { //if train is at platform
-		if (status == PersonStatus.ReadyToBoard && currentPlatform.incomingTrain.status != Train.TrainStatus.BoardingTime) {
-			rb.velocity = jostlingStrength * (platformTarget - transform.position).normalized;	//nudge in direction of platformTarget
-		} else if (status == PersonStatus.MovingToDoor) {
-			if (nmAgent.hasPath) {
-				if (nmAgent.remainingDistance <= currentPlatform.GetNavTargetThreshold()) {	//as more people enter platform then easier to hit target
-					//proximityDistance = currentPlatform.incomingTrain.length;
-					ToggleAgentControl (false);
-					status = PersonStatus.ReadyToBoard;	//may want a better way of checking to confirm boarded
-				}
-			} else {
-				Debug.LogError ("NavMeshAgent has no path: slipped through cracks?");
+		if (currentPlatform && currentPlatform.incomingTrain.status == Train.TrainStatus.BoardingTime) {
+			if (status == PersonStatus.ReadyToBoard) {
+				SetAgentControl (false);
+				trainTarget = GetNewTarget ();
+				status = PersonStatus.Boarding;
+			} else if (status == PersonStatus.Boarding) {
+				rb.AddForce (boardingForce * (trainTarget - transform.position));
 			}
-			//this code handles manual control of people finding space
-//		} else if (status == PersonStatus.ReadyToBoard && Time.time > nextProximityCheck) {
-//			nextProximityCheck = Time.time + checkProximityEvery;
-//			Vector3 newVelocity = rb.velocity, proximityCorrection = Vector3.zero;
-//			RaycastHit hit;
-//			float longestRayLength = 0f;
-//			foreach(Vector3 direction in proximityDirections) {
-//				Physics.Raycast (transform.position, direction, out hit, proximityDistance);
-//				if (hit.distance < proximityDistance && hit.distance > longestRayLength) {
-//					longestRayLength = hit.distance;
-//					proximityCorrection = direction.normalized;
-//					Debug.DrawRay (transform.position,direction.normalized * hit.distance,Color.green,0.1f);
-//				}
-//			}
-//			newVelocity += proximityCorrection * jostlingStrength;
-//			rb.velocity = newVelocity;
-//		} else {
-//			return;
 		}
+		//if they have been waiting for more than 5 seconds they get a nearby location and move.
+		//actually, to ensure that people can be shoved off the platform we should make it physics control trying to reach their assigned destination (and maybe add a NM Obstacle so agents know to avoid them).
 	}
 
-
 	void OnTriggerEnter(Collider coll) {
-		if (status != PersonStatus.Compromised) { //if not compromised
-			Train train = coll.gameObject.GetComponentInParent <Train> ();
+		if (status != PersonStatus.Compromised) {
+			Train train = coll.gameObject.GetComponentInParent <Train> ();	//if this is the front of the train
 			if (train && train.gameObject.GetComponent <Rigidbody> ().velocity.sqrMagnitude > sqrMagDeathVelocity) {
+				status = PersonStatus.Compromised;
 				DeathKnell ();
+			} else if (coll.gameObject.GetComponent <Train> ()) {	//if this is the trigger inside the train carriage
+				status = PersonStatus.Boarded;
+				rb.constraints = RigidbodyConstraints.None;
 			} else {
 				Platform platform = coll.gameObject.GetComponent<Platform> ();
 				if (platform && status == PersonStatus.MovingToPlatform) {
-					RegisterWithPlatform (platform);
+					currentPlatform = platform;
+					if (destination == currentPlatform.nextDeparture) {
+						status = PersonStatus.ReadyToBoard;
+						RegisterWithPlatform ();
+					} else {
+						status = PersonStatus.WrongPlatform;
+					}
 				}
 			}
+		}
+	}
+
+	void OnTriggerExit(Collider coll) {
+		Platform platform = coll.gameObject.GetComponent<Platform> ();
+		if (platform == currentPlatform) {
+			UnregisterWithPlatform ();
 		}
 	}
 		
 	//TODO perhaps some OnTriggerExit stuff when leaving platform... make this all abit simpler
 
-	void DeathKnell() {
-		status = PersonStatus.Compromised;
-		ToggleAgentControl (false);	//turn their kinematic and nmagent off ready for beautiful physics interactions
-		rb.ResetCenterOfMass ();
-		Debug.Log ("Train or compromised person has hit me. Turning into ragdoll");
+	void SetAgentControl(bool turnOn) {
+		if (rb.isKinematic != turnOn) {	//ignore request if they are already set to requested bool
+			rb.isKinematic = turnOn;
+			nmAgent.enabled = turnOn;
+		}
 	}
 
-	void RegisterWithPlatform (Platform platform)
+	Vector3 GetNewTarget() {
+		Vector3 shortestRoute = currentPlatform.targetLocations[0] - transform.position;
+		for (int i=1;i<currentPlatform.targetLocations.Count;i++) {
+			Vector3 route = currentPlatform.targetLocations [i] - transform.position;
+			if (route.magnitude < shortestRoute.magnitude) {
+				shortestRoute = route;
+			}
+		}
+		return shortestRoute;
+	}
+
+	void DeathKnell() {
+		SetAgentControl (false);	//turn their kinematic and nmagent off ready for beautiful physics interactions
+		rb.constraints = RigidbodyConstraints.None;
+		rb.ResetCenterOfMass ();
+	}
+
+	void RegisterWithPlatform ()
 	{
-		currentPlatform = platform;
-		if (destination == platform.nextDeparture) {
-			status = PersonStatus.MovingToDoor;
-			platform.RegisterPerson (this);
-			platformTarget = platform.GetRandomWaitLocation ();
-			nmAgent.SetDestination (platformTarget);
-		}
-		else {
-			status = PersonStatus.WrongPlatform;
-			//send nmAgent back to waiting area or something?
-		}
+		currentPlatform.RegisterPerson (this);
+		platformTarget = currentPlatform.GetNewWaitLocation ();
+		nmAgent.SetDestination (platformTarget);
+	}
+
+	void UnregisterWithPlatform() {
+		currentPlatform.UnregisterPerson (this);
+		//send nmAgent back to waiting area or something?
 	}
 		
-	public void ToggleAgentControl(bool isTurnOn) {
-		rb.isKinematic = isTurnOn;
-		nmAgent.enabled = isTurnOn;
-	}
+
 }
--- Assets/Platforms/Platform.cs
+++ Assets/Platforms/Platform.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,48 +9,85 @@ public class Platform : MonoBehaviour {
 	//atm I envision Platform as fairly dumb, just holding next train details so People can query and get ready
 	//and leaving the overall logic to GameManager who has knowledge of all platforms
 
+	public float waitSpacing = 1f;
 	public string nextDeparture { get; private set; }
 	public Train incomingTrain { get; private set; }
-	public List<Person> peopleAtPlatform = new List<Person> ();
+	public List<Vector3> targetLocations {get; private set;}
 
-	private List<Vector3> waitLocations = new List<Vector3>();
-	private float navTargetThreshold = 0.1f;
+	private List<Person> peopleAtPlatform = new List<Person> ();
+	private List<Vector3> waitLocations = new List<Vector3> ();
+	private Bounds platformTriggerBounds, platformSignalBounds;
 
 	void Start () {
+		platformTriggerBounds = GetComponent <BoxCollider>().bounds;
+		platformSignalBounds = GetComponentInChildren<Signal> ().gameObject.GetComponent <BoxCollider> ().bounds;
 		nextDeparture = "Bristol"; //hard coding
 		incomingTrain = GameManager.GetDeparture (nextDeparture);
-		RecalculateWaitLocations ();
-	}
 
-	void RecalculateWaitLocations() {
-		waitLocations.Clear ();
-		Door[] doors = incomingTrain.GetComponentsInChildren <Door> ();
-		Vector3 newWait;
-		newWait.y = GetComponentInChildren<Signal> ().gameObject.GetComponent <BoxCollider> ().bounds.max.y + 0.5f;
-		newWait.z = transform.position.z;
-		foreach (Door door in doors) {
-			newWait.x = GetComponentInChildren<Signal>().gameObject.GetComponent <BoxCollider> ().bounds.max.x + door.gameObject.transform.localPosition.x;
-			waitLocations.Add (newWait);
+		targetLocations = new List<Vector3> ();
+		RecalculateTargetLocations ();
+		CalculateNewWaitLocations ();
+	}
+	void Update() {
+		if (Input.GetMouseButtonDown (0)) {
+			CalculateNewWaitLocations ();
 		}
 	}
 
-	public Vector3 GetRandomWaitLocation() {
-		return waitLocations [Random.Range (0, waitLocations.Count)];
+	void RecalculateTargetLocations() {
+		targetLocations.Clear ();
+		Door[] doors = incomingTrain.GetComponentsInChildren <Door> ();
+		Vector3 newTarget;
+		newTarget.y = platformSignalBounds.max.y + 0.5f;
+		newTarget.z = platformSignalBounds.center.z;
+		foreach (Door door in doors) {
+			newTarget.x = platformSignalBounds.max.x + door.gameObject.transform.localPosition.x;
+			targetLocations.Add (newTarget);
+		}
 	}
 
-	public float GetNavTargetThreshold() {
-		return navTargetThreshold * peopleAtPlatform.Count;
+	void CalculateNewWaitLocations(){
+		PoissonDiscSampler sampler = new PoissonDiscSampler (platformTriggerBounds.size.x, platformTriggerBounds.size.z, waitSpacing);
+		foreach (Vector2 sample in sampler.Samples()) {
+			Vector3 waitLocation = platformTriggerBounds.min;	//place at the '0,0' location for the generated grid
+			waitLocation.x += sample.x;
+			waitLocation.z += sample.y;
+			NavMeshHit hit;
+			if (NavMesh.SamplePosition (waitLocation, out hit, 0.5f, NavMesh.AllAreas)) {
+				waitLocations.Add (hit.position);
+			}
+		}
 	}
 
+	public Vector3 GetNewWaitLocation() {
+		if (waitLocations.Count == 0) {
+			CalculateNewWaitLocations ();
+		}
+		int randIndex = Random.Range (0, waitLocations.Count);
+		Vector3 waitLocation = waitLocations [randIndex];
+		waitLocations.RemoveAt (randIndex);
+		return waitLocation;
+	}
+		
 	public void RegisterPerson(Person person) {
 		peopleAtPlatform.Add (person);
 	}
 
+	public void UnregisterPerson(Person person) {
+		peopleAtPlatform.Remove (person);	//perhaps the wait locations should be the store for people...
+	}
+
 	void OnDrawGizmos() {
-		if (waitLocations.Count > 0) {
+		if (targetLocations.Count > 0) {
 			Gizmos.color = Color.green;
+			foreach (Vector3 targetLocation in targetLocations) {
+				Gizmos.DrawWireSphere (targetLocation, 1f);
+			}
+		}
+		if (waitLocations.Count > 0) {
+			Gizmos.color = Color.yellow;
 			foreach (Vector3 waitLocation in waitLocations) {
-				Gizmos.DrawWireSphere (waitLocation, 1f);
+				Gizmos.DrawSphere (waitLocation, 0.1f);
 			}
 		}
 	}
